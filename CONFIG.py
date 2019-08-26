ecf21a1eeb9ed941ddb515985d7979c7e24bba0f
Zhuqiang Lu <zhuqianglu@vlan-2641-10-19-81-245.student.wireless.sydney.edu.au>
2019-08-26 13:09:37 +1000

    intern

---
+++ CONFIG.py
@@ -0,0 +1,6 @@
+BASE_URI = 'https://interns.bcgdvsydney.com/'
+KEY_ENDPOINT = 'api/v1/key'
+SUBMIT_ENDPOINT = 'api/v1/submit?apiKey='
+HEADER = {"User-Agent":"Mozilla/5.0"}
+
+APPLICATION = '{"name": "Zhuqiang Lu","email": "zhlu6105@uni.sydney.edu.au"}'
---
+++ internApplication.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import CONFIG
+
+uri = CONFIG.BASE_URI + CONFIG.KEY_ENDPOINT
+#the uri is not trusted
+step1 = requests.get(uri, verify = False, headers=CONFIG.HEADER)
+print(step1.status_code) #to make sure it is 201
+key = step1.json()["key"]
+
+submitURI = CONFIG.BASE_URI + CONFIG.SUBMIT_ENDPOINT + key
+payload = json.loads(CONFIG.APPLICATION)
+step2 = requests.post(submitURI, json = payload, headers = CONFIG.HEADER)
+print(step2.status_code)
+print(step2.json()) 
+# application = json.loads(CONFIG.APPLICATION)
+
